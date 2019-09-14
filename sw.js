15716a6802c92f75ad748b0335fdcee56971ef9d
SuperPixelDev <44777579+SuperPixelDev@users.noreply.github.com>
2019-09-14 21:38:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-importScripts('https://sdk.truepush.com/sdk/sw.js/?key=5c7749e582cc1befeb82d22b')
+const FILES_TO_CACHE = [
+	'/offline.html',
+];
+
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+);
+
+importScripts('https://sdk.truepush.com/sdk/sw.js/?key=5c7749e582cc1befeb82d22b');
