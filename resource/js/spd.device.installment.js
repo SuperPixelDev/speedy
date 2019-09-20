0974b026673fbbfcdc8a339948d558e6618968b0
SuperPixelDev <44777579+SuperPixelDev@users.noreply.github.com>
2019-09-20 17:52:27 +0900

    Add files via upload

--- resource/js/spd.device.installment.js
+++ resource/js/spd.device.installment.js
@@ -10,12 +10,13 @@ var deviceapp = VPSC_GetURL_Param('app', '');
 var deviceapp_ver = VPSC_GetURL_Param('ver', '');
 var deviceapp_os = getMobileOperatingSystem();
 if (VPSC_Cookie_Get('notice_donotshow') == '') {
-	if (deviceapp !== 'true' && deviceapp_os == 'Android') { $('#VX_Notification').modal(); }
+	if (deviceapp !== 'true' && deviceapp_os == 'Android') { setTimeout(function(){ $('#VX_Notification').modal(); }, 3000); }
 }
 
 /* WeixinJSBridge */
 document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
 	document.getElementById('span_speedy_api_alert_close').innerHTML = '';
+	document.getElementById('span_speedy_download').innerHTML = 'You cannot download the file as you are using WeChat Browser.';
 	$('#VX_Notification').modal('hide');
 	SPEEDYSDK_C_Alert_Simple('WeChat Browser Detected!', '<img src="//qdcwapp.com/speedy/file/img/wechat_browser.jpg" style="width:100%" /><p style="margin-top:20px">Please open SPEEDY with the Internet Browser installed on your device!</p>');
 	WeixinJSBridge.invoke('showOptionMenu',{},function(res){});
